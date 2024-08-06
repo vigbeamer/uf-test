f46f09970ed3a588b3acbd8913c49b61a0a738b5
vigbeamer <vignesh.shanmugavelu@getbeamer.com>
2024-08-06 09:40:24 +0530

    remove package folder

--- package/test/ssr.test.ts
+++
@@ -1,6 +0,0 @@
-import userflow from '../src/userflow'
-
-test('can load userflow in Node land', () => {
-  expect(typeof window).toBe('undefined')
-  expect(typeof userflow.init).toBe('function')
-})
